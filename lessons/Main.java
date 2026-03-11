9e4eb369f81ac7fc2807a40e00e7d682871e50fa
auryxn <ioda.aliaksei@student.ehu.lt>
2026-03-11 22:23:57 +0200

    1 lesson

---
+++ lessons/Main.java
@@ -0,0 +1,9 @@
+package homework.lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Alex";
+        int age = 18;
+        System.out.println(name + " " + age);
+    }
+}
