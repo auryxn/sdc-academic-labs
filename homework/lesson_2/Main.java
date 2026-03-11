9c2bd53e92b7f6a6e39ddd292c06ee3fb635c1c5
auryxn <ioda.aliaksei@student.ehu.lt>
2026-03-11 22:38:28 +0200

    2 lesson

---
+++ homework/lesson_2/Main.java
@@ -0,0 +1,12 @@
+package homework.lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+        int targetIncome = 10000;
+        int daysInMonth = 22;
+
+        dailyRate = targetIncome / daysInMonth;
+        System.out.println(targetIncome);
+        System.out.println(%targetIncome);
+    }
+}
