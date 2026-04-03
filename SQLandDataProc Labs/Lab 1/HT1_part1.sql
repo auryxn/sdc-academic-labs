7c99a24fbf5e9d3193a1c7118f412b459b9ef4cd
auryxn <auryxn@gmail.com>
2026-04-03 06:24:09 +0000

    Initialize SQL and Data Processing Lab 1

---
+++ SQLandDataProc Labs/Lab 1/HT1_part1.sql
@@ -0,0 +1,41 @@
+/*
+Use the script from the file HT1_part1.sql to create the DWH tables and the staging table. 
+Populate the Dim_Gym and Dim_Member tables, and add initial data to the Staging table.
+*/
+
+drop table if exists Fact_Visit;
+drop table if exists Dim_Member;
+drop table if exists Dim_Gym;
+drop table if exists Dim_Date;
+drop table if exists Staging_Gym_visit;
+drop type if exists day_part_enum;
+
+create table Dim_Member(
+    member_id serial primary key,
+    personal_code text not null unique,
+    last_name text not null,
+    first_name text not null
+);
+
+create table Dim_Gym(
+    gym_id serial primary key,
+    gym_code text not null unique
+);
+
+create table Dim_Date(
+    date_key date primary key,  
+    day_number_of_month int not null,
+    month_number_of_year int not null,
+    year_number int not null,
+    day_name text not null,
+    month_name text not null    
+);
+
+create type day_part_enum as enum ('Morning', 'Day', 'Evening');
+
+create table Fact_Visit(
+    visit_id serial primary key,
+    gym_id int not null references Dim_Gym(gym_id),
+    member_id int not null references Dim_Member(member_id),
+    visit_date_key date not null references Dim_Date(date_key)
+);
