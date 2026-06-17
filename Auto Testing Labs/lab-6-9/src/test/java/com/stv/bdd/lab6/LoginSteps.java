eb540be889db7de1f6b39cd3fc25f71be83152e4
auryxn <auryxn@gmail.com>
2026-06-17 12:16:36 +0000

    Fix LoginSteps: restore WebElement/By imports, use getDriver()

--- Auto Testing Labs/lab-6-9/src/test/java/com/stv/bdd/lab6/LoginSteps.java
+++ Auto Testing Labs/lab-6-9/src/test/java/com/stv/bdd/lab6/LoginSteps.java
@@ -2,20 +2,21 @@ package com.stv.bdd.stepdefinitions;
 
 import com.stv.design.designtests.BasicTest;
 import com.stv.factory.factorypages.FactoryMainPage;
-import com.stv.factory.factorypages.FactoryRegisterPage;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 public class LoginSteps extends BasicTest {
 
     private FactoryMainPage mainPage = new FactoryMainPage();
+    private java.util.function.Supplier<org.openqa.selenium.WebDriver> driverSupplier = this::getDriver;
 
     @When("I enter username {string} and password {string}")
     public void iEnterUsernameAndPassword(String username, String password) {
-        WebElement usernameField = driver.findElement(By.name("username"));
-        WebElement passwordField = driver.findElement(By.name("password"));
+        WebElement usernameField = getDriver().findElement(By.name("username"));
+        WebElement passwordField = getDriver().findElement(By.name("password"));
         usernameField.clear();
         usernameField.sendKeys(username);
         passwordField.clear();
@@ -24,13 +25,13 @@ public class LoginSteps extends BasicTest {
 
     @When("I click the Login button")
     public void iClickTheLoginButton() {
-        WebElement loginButton = driver.findElement(By.cssSelector("input[value='Log In']"));
+        WebElement loginButton = getDriver().findElement(By.cssSelector("input[value='Log In']"));
         loginButton.click();
     }
 
     @Then("I should see the Account Overview page with a welcome message")
     public void iShouldSeeTheAccountOverviewPageWithAWelcomeMessage() {
-        WebElement welcomeMessage = driver.findElement(By.className("smallText"));
+        WebElement welcomeMessage = getDriver().findElement(By.className("smallText"));
         Assert.assertTrue(welcomeMessage.isDisplayed(), "Welcome message should be displayed");
         Assert.assertTrue(welcomeMessage.getText().contains("Welcome"), "Welcome message should contain 'Welcome'");
     }
